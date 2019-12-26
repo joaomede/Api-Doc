967d878f80b9cde7aba6a9457cf6023004e7360f
Joao Medeiros <joaoserj@gmail.com>
2019-12-26 15:39:51 -0300

    p- fixed routes auth

--- api/routes/RouterAuth.ts
+++ api/routes/RouterAuth.ts
@@ -4,15 +4,15 @@ import { Router } from 'express'
 
 const routes = Router()
 
-routes.post('/api/api/auth/login', validate.login, c.auth)
-routes.post('/api/api/auth/register', validate.register, c.register)
-routes.post('/auth/forgot', c.forgotPassword)
-routes.post('/auth/reset', c.resetPassword)
-routes.post('/auth/alterarsenha', c.alterarSenha)
+routes.post('/api/auth/login', validate.login, c.auth)
+routes.post('/api/auth/register', validate.register, c.register)
+routes.post('/api/auth/forgot', c.forgotPassword)
+routes.post('/api/auth/reset', c.resetPassword)
+routes.post('api/auth/alterarsenha', c.alterarSenha)
 
 // Danger
-routes.put('/auth/atualizarfuncao', c.atualizaFuncao)
-routes.get('/auth/listausuarios', c.listaUsuarios)
-routes.delete('/auth/apagaruser/:refId', c.apagarUser)
+routes.put('/api//auth/atualizarfuncao', c.atualizaFuncao)
+routes.get('/api/auth/listausuarios', c.listaUsuarios)
+routes.delete('/api/auth/apagaruser/:refId', c.apagarUser)
 
 export default routes
