4383844924048327039b44ce29fa7e76f9670dae
joao <joaoserj@gmail.com>
2019-12-27 18:46:41 -0300

    p- clear all routes auth disabled

--- api/routes/RouterAuth.ts
+++ api/routes/RouterAuth.ts
@@ -6,13 +6,5 @@ const routes = Router()
 
 routes.post('/api/auth/login', validate.login, c.auth)
 routes.post('/api/auth/register', validate.register, c.register)
-routes.post('/api/auth/forgot', c.forgotPassword)
-routes.post('/api/auth/reset', c.resetPassword)
-routes.post('api/auth/alterarsenha', c.alterarSenha)
-
-// Danger
-routes.put('/api//auth/atualizarfuncao', c.atualizaFuncao)
-routes.get('/api/auth/listausuarios', c.listaUsuarios)
-routes.delete('/api/auth/apagaruser/:refId', c.apagarUser)
 
 export default routes
