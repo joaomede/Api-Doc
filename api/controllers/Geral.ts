1b9bccab37abaec93284f1d2875df21d4f85e27a
joao <joaoserj@gmail.com>
2019-12-26 21:32:28 -0300

    p- added public querys

--- api/controllers/Geral.ts
+++ api/controllers/Geral.ts
@@ -2,25 +2,59 @@ import { knex } from '../db/connection'
 import { NewRequest } from '../interface/NewRequest'
 import resp from 'resp-express'
 import { Response } from 'express'
+import knexPopulate from 'knex-populate'
+
 class Geral {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   public async getPublicDoc (req: NewRequest, res: Response): Promise<any> {
     const { id } = req.params
 
     try {
-      const doc = await knex('api').where({ id: id })
+      // const doc = await knex('api').where({ id: id })
 
-      if (doc.length === 0) {
-        resp.returnErrorMessage(res, 'A api que você está tentando acessar não existe')
+      const api = await knex('api').where({ id: id })
+      const endPoint = await knex('endpoint').where({ apiIdFk: id })
+      api[0].endpoint = endPoint
+
+      if (api.length === 0) {
+        resp.returnErrorMessage(res, 'A api que você está tentando acessar não foi encontrada')
       } else {
-        if (doc[0].isPublic === 1 || doc[0].isPublic === true) {
-          resp.returnSucessObject(res, doc[0])
+        if (api[0].isPublic === 1 || api[0].isPublic === true) {
+          resp.returnSucessObject(res, api[0])
         } else {
           resp.returnErrorMessage(res, 'Você não tem autorização para acessar essa documentação')
         }
       }
     } catch (error) {
       resp.returnErrorMessage(res, 'Erro ao tentar carregar a documentação')
+    }
+  }
+
+  public async getPublicVerb (req: NewRequest, res: Response): Promise<any> {
+    const { endPointId } = req.params
+
+    try {
+      const verbAndCodes = await knexPopulate(knex, 'verb')
+        .find({ endPointIdFK: endPointId })
+        .populate('codesresp', 'verbIdFk', 'codesresp')
+        .exec()
+
+      if (verbAndCodes.length === 0) {
+        resp.returnErrorMessage(res, 'Não há verbos disponíveis')
+      } else {
+        resp.returnSucessObject(res, verbAndCodes)
+      }
+    } catch (error) {
+      resp.returnErrorMessage(res, 'Erro ao tentar expandir')
+    }
+  }
+
+  public async indexPreviewAllPublicDocs (req: NewRequest, res: Response): Promise<any> {
+    try {
+      const allPublicList = await knex('api').select().where({ isPublic: true })
+      resp.returnSucessObject(res, allPublicList)
+    } catch (error) {
+      resp.returnErrorMessage(res, 'Erro ao tentar listar documentação')
     }
   }
 }
--- api/routes/RouterGeral.ts
+++ api/routes/RouterGeral.ts
@@ -4,5 +4,7 @@ import { Router } from 'express'
 const routes = Router()
 
 routes.get('/api/geral/getpublicdoc/:id', c.getPublicDoc)
+routes.get('/api/geral/getpublicverb/:endPointId', c.getPublicVerb)
+routes.get('/api/geral/indexpreviewpublicdoc', c.indexPreviewAllPublicDocs)
 
 export default routes
