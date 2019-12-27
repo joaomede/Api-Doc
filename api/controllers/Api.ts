aa70b8aaee18253f01745e36fe89b3eaa93d609d
Joao Medeiros <joaoserj@gmail.com>
2019-12-27 16:49:52 -0300

    p- added 2 new methods in controller api

--- api/controllers/Api.ts
+++ api/controllers/Api.ts
@@ -2,6 +2,7 @@ import { knex } from '../db/connection'
 import { NewRequest } from '../interface/NewRequest'
 import { Response } from 'express'
 import resp from 'resp-express'
+import knexPopulate from 'knex-populate'
 
 class Api {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -80,6 +81,55 @@ class Api {
       resp.returnSucessMessage(res, 'Documentação deletada com sucesso')
     } catch (error) {
       resp.returnErrorMessage(res, 'Erro ao tentar deletar documentação')
+    }
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  public async getApiAndEndPoints (req: NewRequest, res: Response): Promise<any> {
+    const { id } = req.params
+
+    try {
+      // const doc = await knex('api').where({ id: id })
+
+      const api = await knex('api').where({ id: id })
+      const endPoint = await knex('endpoint').where({ apiIdFk: id })
+      api[0].endpoint = endPoint
+
+      if (api.length === 0) {
+        resp.returnErrorMessage(res, 'A api que você está tentando acessar não foi encontrada')
+      } else {
+        if (api[0].userIdFk === req.userId) {
+          resp.returnSucessObject(res, api[0])
+        } else {
+          resp.returnErrorMessage(res, 'Você não tem autorização para acessar essa documentação')
+        }
+      }
+    } catch (error) {
+      resp.returnErrorMessage(res, 'Erro ao tentar carregar a documentação')
+    }
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  public async getVerbsAndCodes (req: NewRequest, res: Response): Promise<any> {
+    const { endPointId } = req.params
+
+    try {
+      const verbAndCodes = await knexPopulate(knex, 'verb')
+        .find({ endPointIdFK: endPointId })
+        .populate('codesresp', 'verbIdFk', 'codesresp')
+        .exec()
+
+      if (verbAndCodes.length === 0) {
+        resp.returnErrorMessage(res, 'Não há verbos disponíveis')
+      } else {
+        if (verbAndCodes[0].userIdFk === req.userId) {
+          resp.returnSucessObject(res, verbAndCodes)
+        } else {
+          resp.returnErrorMessage(res, 'Você não tem autorização para acessar essa documentação')
+        }
+      }
+    } catch (error) {
+      resp.returnErrorMessage(res, 'Erro ao tentar expandir')
     }
   }
 }
