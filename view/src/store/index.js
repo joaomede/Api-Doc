dbf529a65b6dd3b5492f7ac8fd532a0cc71e20a2
Joao Medeiros <joaoserj@gmail.com>
2019-12-27 10:00:02 -0300

    p- update store checkVersion and boot

--- view/src/store/index.js
+++ view/src/store/index.js
@@ -1,10 +1,13 @@
-import Vue from 'vue'
+import Vue, { ssrContext } from 'vue'
 import Vuex from 'vuex'
+import { Cookies } from 'quasar'
+import { db } from '../boot/firebase'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
     user: null,
+    version: null,
     apisList: null
   },
   getters: {
@@ -13,20 +16,63 @@ export default new Vuex.Store({
     },
     getListApis: state => {
       return state.apisList
+    },
+    getVersion (state) {
+      return state.version
     }
   },
   mutations: {
-    setUser: state => {
+    async boot (state) {
+      const cookies = process.env.SERVER ? Cookies.parseSSR(ssrContext) : Cookies // otherwise we're on client
+      const user = await cookies.get('user')
 
+      if (user !== undefined) {
+        state.user = {
+          id: user.id,
+          email: user.email,
+          name: user.name,
+          headers: user.headers
+        }
+      } else {
+        state.user = {
+          id: null,
+          email: 'email@email.com',
+          name: 'Nothing',
+          headers: {
+            auth: 'nothing nothing'
+          }
+        }
+      }
+    },
+    async versionCheck (state) {
+      state.version = process.env.VERSION
+      state.appFeedId = process.env.APPFEED
+      let version
+
+      db
+        .collection('app')
+        .doc(state.appFeedId)
+        .collection('feed')
+        .where('novo', '==', true)
+        .onSnapshot(querySnapshot => {
+          querySnapshot.forEach(doc => {
+            version = doc.data().tituloPostagem
+          })
+          if (state.version !== version) {
+            window.location.reload(true)
+          }
+        })
     },
     setApisList (state, objeto) {
       state.apisList = objeto
     }
-
   },
   actions: {
-    setUser: context => {
-      context.commit('setUser')
+    boot ({ commit }) {
+      commit('boot')
+    },
+    versionCheck ({ commit }) {
+      commit('versionCheck')
     },
     setApisList ({ commit }, apislist) {
       commit('setApisList', apislist)
