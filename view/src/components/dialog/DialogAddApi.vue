92cdad86a321d410d6bacbdcbe03ef8e06b644bd
joao <joaoserj@gmail.com>
2019-12-27 21:09:04 -0300

    p- Added components Dialog Add New Entity's

---
+++ view/src/components/dialog/DialogAddApi.vue
@@ -0,0 +1,120 @@
+<template>
+  <q-dialog
+    v-model="dialogPopup"
+    @hide="eventClose()"
+  >
+    <q-card>
+      <q-card-section>
+        <div class="text-h6">
+          Informe os dados da sua API
+        </div>
+      </q-card-section>
+
+      <q-card-section>
+        <q-form class="q-gutter-md">
+          <q-input
+            v-model="form.apiName"
+            label="Informe um nome para a nova documentação"
+            required
+          />
+        </q-form>
+      </q-card-section>
+
+      <q-card-section>
+        <q-form class="q-gutter-md">
+          <q-input
+            v-model="form.descriptionApi"
+            label="Informe uma descrição"
+            required
+          />
+        </q-form>
+      </q-card-section>
+
+      <q-card-section>
+        <q-form class="q-gutter-md">
+          <q-input
+            v-model="form.version"
+            label="Informe a versão atual da api"
+            required
+          />
+        </q-form>
+      </q-card-section>
+
+      <q-card-section>
+        <div class="q-gutter-sm">
+          <q-checkbox
+            v-model="form.isPublic"
+            keep-color
+            label="Ela é publica?"
+            color="blue"
+          />
+        </div>
+      </q-card-section>
+
+      <q-card-section align="center">
+        <q-btn
+          class="q-ma-xs"
+          color="black"
+          @click.stop="eventClose()"
+        >
+          Voltar
+        </q-btn>
+        <q-btn
+          class="q-ma-xs"
+          color="green"
+          @click="save"
+        >
+          Sim
+        </q-btn>
+      </q-card-section>
+
+      <q-card-section />
+    </q-card>
+  </q-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    dialog: Boolean
+  },
+  data () {
+    return {
+      form: {
+        apiName: '',
+        descriptionApi: '',
+        version: '',
+        isPublic: false
+      },
+      dialogPopup: this.dialog
+    }
+  },
+  watch: {
+    dialog: 'update'
+  },
+  methods: {
+    update () {
+      this.dialogPopup = this.dialog
+    },
+    eventClose () {
+      this.dialogPopup = false
+      this.$emit('eventClose')
+      this.reset()
+    },
+    save () {
+      this.$emit('save', this.form)
+    },
+    reset () {
+      this.form = {
+        apiName: '',
+        descriptionApi: '',
+        version: '',
+        isPublic: false
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ view/src/components/dialog/DialogAddNewEndPoint.vue
@@ -0,0 +1,95 @@
+<template>
+  <q-dialog
+    v-model="dialogPopup"
+    @hide="eventClose()"
+  >
+    <q-card>
+      <q-card-section>
+        <div class="text-h6">
+          Informe os dados do novo Endpoint
+        </div>
+      </q-card-section>
+
+      <q-card-section>
+        <q-form class="q-gutter-md">
+          <q-input
+            v-model="form.nameEndPointsType"
+            label="Informe o nome do Endpoint"
+            required
+          />
+        </q-form>
+      </q-card-section>
+
+      <q-card-section>
+        <q-form class="q-gutter-md">
+          <q-input
+            v-model="form.descriptionEndPonitsType"
+            label="Informe a descrição do Endpoint"
+            required
+          />
+        </q-form>
+      </q-card-section>
+
+      <q-card-section align="center">
+        <q-btn
+          class="q-ma-xs"
+          color="black"
+          @click.stop="eventClose()"
+        >
+          Voltar
+        </q-btn>
+        <q-btn
+          class="q-ma-xs"
+          color="green"
+          @click="save"
+        >
+          Sim
+        </q-btn>
+      </q-card-section>
+
+      <q-card-section />
+    </q-card>
+  </q-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    dialog: Boolean
+  },
+  data () {
+    return {
+      form: {
+        nameEndPointsType: '',
+        descriptionEndPonitsType: ''
+      },
+      dialogPopup: this.dialog
+    }
+  },
+  watch: {
+    dialog: 'update'
+  },
+  methods: {
+    update () {
+      this.dialogPopup = this.dialog
+    },
+    eventClose () {
+      this.dialogPopup = false
+      this.$emit('eventClose')
+      this.reset()
+    },
+    save () {
+      this.$emit('save', this.form)
+    },
+    reset () {
+      this.form = {
+        nameEndPointsType: '',
+        descriptionEndPonitsType: ''
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
