f50aaf53ceeff437696b1b2f1f4d1a088d8e62f0
joao <joaoserj@gmail.com>
2019-12-27 21:11:18 -0300

    p- created main menu component

---
+++ view/src/components/menu/MainMenu.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="text-h5">
+    {{ title }}
+    <q-card class="my-card">
+      <div
+        class="q-gutter-xs row"
+        style="justify-content: center;"
+      >
+        <div
+          class="col-xs-auto"
+        >
+          <q-btn
+            v-ripple
+            class="btnMenu"
+            to="/publicdocs"
+            dense
+          >
+            <q-icon
+              name="list"
+              class="text-black"
+              style="font-size: 4.4em;"
+            />
+            <div class="text-p">
+              Public API's
+            </div>
+          </q-btn>
+        </div>
+
+        <div
+          class="col-xs-auto"
+        >
+          <q-btn
+            v-ripple
+            class="btnMenu"
+            to="/privatedocs"
+            dense
+          >
+            <q-icon
+              name="security"
+              class="text-black"
+              style="font-size: 4.4em;"
+            />
+            <div class="text-p">
+              Private API's
+            </div>
+          </q-btn>
+        </div>
+      </div>
+    </q-card>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
