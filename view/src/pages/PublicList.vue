8ed408905d9bcf3f8b6fb2ba4831326270772ffc
joao <joaoserj@gmail.com>
2019-12-27 21:09:48 -0300

    p- created Public list api's

---
+++ view/src/pages/PublicList.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="centralDiv q-pa-xs text-center">
+    <q-card class="my-card text-center">
+      <q-list
+        bordered
+        style="max-width: 900px; margin: auto;"
+      >
+        <div class="text-h6">
+          Lista de Documentação de API publicas
+        </div>
+        <q-separator spaced />
+        <q-item
+          v-for="item in listOfApis"
+          :key="item.idApi"
+          v-ripple
+          clickable
+          style="font-size: 18px;"
+          @click="toPageCompletePublicDoc(item)"
+        >
+          <q-item-section
+            avatar
+            top
+          >
+            <i
+              class="far fa-file-alt text-black"
+              style="font-size: 3em;"
+            />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label lines="5">
+              {{ item.apiName }}
+            </q-item-label>
+
+            <q-item-label caption>
+              {{ item.descriptionApi }}
+            </q-item-label>
+          </q-item-section>
+
+          <q-separator spaced />
+        </q-item>
+      </q-list>
+    </q-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PublicList',
+  data () {
+    return {
+      listOfApis: []
+    }
+  },
+  computed: {
+  },
+  created () {
+    this.init()
+  },
+  methods: {
+    async init () {
+      this.indexPublicListDocs()
+    },
+    async indexPublicListDocs () {
+      try {
+        const result = await this.$axios.get('api/geral/listallpublicapi', { headers: this.user.headers })
+        this.listOfApis = await result.data
+        console.log(await this.listOfApis)
+      } catch (error) {
+        this.$notify('Erro ao carregar lista de documentação publica', 'red')
+      }
+    },
+    toPageCompletePublicDoc (item) {
+      this.$router.push({ name: 'DocView', params: { id: ('' + item.id) } })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
