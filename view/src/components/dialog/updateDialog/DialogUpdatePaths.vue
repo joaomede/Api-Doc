190894207ce9433a2bb0f9a1320eb8fd11765f31
joao <joaoserj@gmail.com>
2019-12-29 09:37:22 -0300

    p- incremented dialog path update

---
+++ view/src/components/dialog/updateDialog/DialogUpdatePaths.vue
@@ -0,0 +1,177 @@
+<template>
+  <q-dialog
+    v-model="dialogPopup"
+    @hide="eventClose()"
+  >
+    <q-card>
+      <q-card-section>
+        <div class="text-h6">
+          Informe os dados desse tipo de requisição
+        </div>
+      </q-card-section>
+
+      <q-card-section>
+        <q-form class="q-gutter-md">
+          <q-input
+            v-model="form.verbType"
+            label="Informe o tipo de verbo, ex.: POST"
+            required
+          />
+        </q-form>
+      </q-card-section>
+
+      <q-card-section>
+        <q-form class="q-gutter-md">
+          <q-input
+            v-model="form.descriptionVerb"
+            label="Informe a descrição"
+            required
+          />
+        </q-form>
+      </q-card-section>
+
+      <q-card-section>
+        <q-form class="q-gutter-md">
+          <q-input
+            v-model="form.path"
+            label="Informe o endereço do path, ex.: api/login"
+            required
+          />
+        </q-form>
+      </q-card-section>
+
+      <q-card-section>
+        <q-form class="q-gutter-md">
+          <q-input
+            v-model="form.parameter"
+            label="Informe os parametros"
+            required
+          />
+        </q-form>
+      </q-card-section>
+
+      <q-card-section>
+        <q-form class="q-gutter-md">
+          <q-input
+            v-model="form.paramsType"
+            label="Informe o tipo do parametro"
+            required
+          />
+        </q-form>
+      </q-card-section>
+
+      <q-card-section>
+        <q-form class="q-gutter-md">
+          <q-input
+            v-model="form.verbValue"
+            label="Informe os valores"
+            required
+          />
+        </q-form>
+      </q-card-section>
+
+      <q-card-section>
+        <q-form class="q-gutter-md">
+          <q-input
+            v-model="form.respValue"
+            label="Informe o valor da resposta"
+            required
+          />
+        </q-form>
+      </q-card-section>
+
+      <q-card-section>
+        <q-form class="q-gutter-md">
+          <q-input
+            v-model="form.dataType"
+            label="Informe o tipo dos dados que retornarão"
+            required
+          />
+        </q-form>
+      </q-card-section>
+
+      <q-card-section align="center">
+        <q-btn
+          class="q-ma-xs"
+          color="black"
+          @click.stop="eventClose()"
+        >
+          Voltar
+        </q-btn>
+        <q-btn
+          class="q-ma-xs"
+          color="green"
+          @click="save"
+        >
+          Sim
+        </q-btn>
+      </q-card-section>
+
+      <q-card-section />
+    </q-card>
+  </q-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    dialog: Boolean,
+    pathForm: {
+      type: Object,
+      default: function () {
+        return {}
+      }
+    }
+  },
+  data () {
+    return {
+      form: {
+        verbType: '',
+        path: '',
+        parameter: '',
+        verbValue: '',
+        descriptionVerb: '',
+        paramsType: '',
+        respValue: '',
+        dataType: ''
+      },
+      dialogPopup: this.dialog
+    }
+  },
+  watch: {
+    dialog: 'update',
+    pathForm: 'updateForm'
+  },
+  methods: {
+    update () {
+      this.dialogPopup = this.dialog
+    },
+    updateForm () {
+      this.form = this.pathForm
+    },
+    eventClose () {
+      this.dialogPopup = false
+      this.$emit('eventClose')
+      this.reset()
+    },
+    save () {
+      this.$emit('save', this.form)
+    },
+    reset () {
+      this.form = {
+        verbType: '',
+        path: '',
+        parameter: '',
+        verbValue: '',
+        descriptionVerb: '',
+        paramsType: '',
+        respValue: '',
+        dataType: ''
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
