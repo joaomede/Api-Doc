e654cb7ef8b72a2640bf94b8a7ad236eff635716
joao <joaoserj@gmail.com>
2019-12-29 09:35:12 -0300

    p- incremented compoenent updateDialogApi

---
+++ view/src/components/dialog/updateDialog/DialogUpdateApi.vue
@@ -0,0 +1,154 @@
+<template>
+  <q-dialog
+    v-model="dialogPopup"
+    @hide="eventClose()"
+  >
+    <q-card>
+      <q-card-section>
+        <div class="text-h6">
+          Informe os dados da sua API
+        </div>
+      </q-card-section>
+
+      <q-card-section>
+        <q-form class="q-gutter-md">
+          <q-input
+            v-model="form.apiName"
+            label="Informe um nome para a nova documentação"
+            required
+          />
+        </q-form>
+      </q-card-section>
+
+      <q-card-section>
+        <q-form class="q-gutter-md">
+          <q-input
+            v-model="form.descriptionApi"
+            label="Informe uma descrição"
+            required
+          />
+        </q-form>
+      </q-card-section>
+
+      <q-card-section>
+        <q-form class="q-gutter-md">
+          <q-input
+            v-model="form.version"
+            label="Informe a versão atual da api"
+            required
+          />
+        </q-form>
+      </q-card-section>
+
+      <q-card-section>
+        <q-form class="q-gutter-md">
+          <q-input
+            v-model="form.email"
+            label="Informe um e-mail para contato"
+            required
+          />
+        </q-form>
+      </q-card-section>
+
+      <q-card-section>
+        <q-form class="q-gutter-md">
+          <q-input
+            v-model="form.license"
+            label="Informe o tipo de licença ou link"
+            required
+          />
+        </q-form>
+      </q-card-section>
+
+      <q-card-section>
+        <div class="q-gutter-sm">
+          <q-checkbox
+            v-model="form.isPublic"
+            keep-color
+            label="Ela é publica?"
+            color="blue"
+          />
+        </div>
+      </q-card-section>
+
+      <q-card-section align="center">
+        <q-btn
+          class="q-ma-xs"
+          color="black"
+          @click.stop="eventClose()"
+        >
+          Voltar
+        </q-btn>
+        <q-btn
+          class="q-ma-xs"
+          color="green"
+          @click="save"
+        >
+          Sim
+        </q-btn>
+      </q-card-section>
+
+      <q-card-section />
+    </q-card>
+  </q-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    dialog: Boolean,
+    apiForm: {
+      type: Object,
+      default: function () {
+        return {}
+      }
+    }
+  },
+  data () {
+    return {
+      form: {
+        apiName: '',
+        descriptionApi: '',
+        version: '',
+        email: '',
+        license: '',
+        isPublic: false
+      },
+      dialogPopup: this.dialog
+    }
+  },
+  watch: {
+    dialog: 'update',
+    apiForm: 'updateForm'
+  },
+  methods: {
+    update () {
+      this.dialogPopup = this.dialog
+    },
+    updateForm () {
+      this.form = this.apiForm
+    },
+    eventClose () {
+      this.dialogPopup = false
+      this.$emit('eventClose')
+      this.reset()
+    },
+    save () {
+      this.$emit('save', this.form)
+    },
+    reset () {
+      this.form = {
+        apiName: '',
+        descriptionApi: '',
+        version: '',
+        email: '',
+        license: '',
+        isPublic: false
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
