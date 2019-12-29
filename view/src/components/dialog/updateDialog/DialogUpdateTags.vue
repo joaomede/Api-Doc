5dcccb170403e08e18cbe990268581bb8a3d110c
joao <joaoserj@gmail.com>
2019-12-29 09:50:43 -0300

    p- incremented tag update components

---
+++ view/src/components/dialog/updateDialog/DialogUpdateTags.vue
@@ -0,0 +1,105 @@
+<template>
+  <q-dialog
+    v-model="dialogPopup"
+    @hide="eventClose()"
+  >
+    <q-card>
+      <q-card-section>
+        <div class="text-h6">
+          Informe um nome e uma descrição para esta tag
+        </div>
+      </q-card-section>
+
+      <q-card-section>
+        <q-form class="q-gutter-md">
+          <q-input
+            v-model="form.nameTag"
+            label="Informe o nome do Tag"
+            required
+          />
+        </q-form>
+      </q-card-section>
+
+      <q-card-section>
+        <q-form class="q-gutter-md">
+          <q-input
+            v-model="form.descriptionTag"
+            label="Informe a descrição do Tag"
+            required
+          />
+        </q-form>
+      </q-card-section>
+
+      <q-card-section align="center">
+        <q-btn
+          class="q-ma-xs"
+          color="black"
+          @click.stop="eventClose()"
+        >
+          Voltar
+        </q-btn>
+        <q-btn
+          class="q-ma-xs"
+          color="green"
+          @click="save"
+        >
+          Sim
+        </q-btn>
+      </q-card-section>
+
+      <q-card-section />
+    </q-card>
+  </q-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    dialog: Boolean,
+    tagForm: {
+      type: Object,
+      default: function () {
+        return {}
+      }
+    }
+  },
+  data () {
+    return {
+      form: {
+        nameTag: '',
+        descriptionTag: ''
+      },
+      dialogPopup: this.dialog
+    }
+  },
+  watch: {
+    dialog: 'update',
+    tagForm: 'updateForm'
+  },
+  methods: {
+    update () {
+      this.dialogPopup = this.dialog
+    },
+    updateForm () {
+      this.form = this.tagForm
+    },
+    eventClose () {
+      this.dialogPopup = false
+      this.$emit('eventClose')
+      this.reset()
+    },
+    save () {
+      this.$emit('save', this.form)
+    },
+    reset () {
+      this.form = {
+        nameTag: '',
+        descriptionTag: ''
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
