3ab676c6c41c147637f60af2ed8089c0c171ff3e
joao <joaoserj@gmail.com>
2019-12-29 09:39:15 -0300

    p- incremented dialog update responses

---
+++ view/src/components/dialog/updateDialog/DialogUpdateResponses.vue
@@ -0,0 +1,129 @@
+<template>
+  <q-dialog
+    v-model="dialogPopup"
+    @hide="eventClose()"
+  >
+    <q-card>
+      <q-card-section>
+        <div class="text-h6">
+          Informe os dados do código de resposta
+        </div>
+      </q-card-section>
+
+      <q-card-section>
+        <q-form class="q-gutter-md">
+          <q-input
+            v-model="form.typeCode"
+            label="Informe o tipo de código"
+            required
+          />
+        </q-form>
+      </q-card-section>
+
+      <q-card-section>
+        <q-form class="q-gutter-md">
+          <q-input
+            v-model="form.reason"
+            label="Informe o motivo do retorno"
+            required
+          />
+        </q-form>
+      </q-card-section>
+
+      <q-card-section>
+        <q-form class="q-gutter-md">
+          <q-input
+            v-model="form.responseModel"
+            label="Informe o modelo da resposta"
+            required
+          />
+        </q-form>
+      </q-card-section>
+
+      <q-card-section>
+        <q-form class="q-gutter-md">
+          <q-input
+            v-model="form.headers"
+            label="Informe o cabeçalho"
+            required
+          />
+        </q-form>
+      </q-card-section>
+
+      <q-card-section align="center">
+        <q-btn
+          class="q-ma-xs"
+          color="black"
+          @click.stop="eventClose()"
+        >
+          Voltar
+        </q-btn>
+        <q-btn
+          class="q-ma-xs"
+          color="green"
+          @click="save"
+        >
+          Sim
+        </q-btn>
+      </q-card-section>
+
+      <q-card-section />
+    </q-card>
+  </q-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    dialog: Boolean,
+    responseForm: {
+      type: Object,
+      default: function () {
+        return {}
+      }
+    }
+  },
+  data () {
+    return {
+      form: {
+        typeCode: '',
+        reason: '',
+        responseModel: '',
+        headers: ''
+      },
+      dialogPopup: this.dialog
+    }
+  },
+  watch: {
+    dialog: 'update',
+    responseForm: 'updateForm'
+  },
+  methods: {
+    update () {
+      this.dialogPopup = this.dialog
+    },
+    updateForm () {
+      this.form = this.responseForm
+    },
+    eventClose () {
+      this.dialogPopup = false
+      this.$emit('eventClose')
+      this.reset()
+    },
+    save () {
+      this.$emit('save', this.form)
+    },
+    reset () {
+      this.form = {
+        typeCode: '',
+        reason: '',
+        responseModel: '',
+        headers: ''
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
