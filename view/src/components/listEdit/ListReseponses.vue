b72620121b07375d3efa9ee3246bec300aebfbdb
Joao Medeiros <joaoserj@gmail.com>
2019-12-30 14:55:07 -0300

    p- component response

---
+++ view/src/components/listEdit/ListReseponses.vue
@@ -0,0 +1,85 @@
+<template>
+  <!-- vFor Responses -->
+  <q-card>
+    <q-card-section>
+      <q-expansion-item
+        icon="
+        a"
+        :label="responses.typeCode"
+        header-class="a"
+      >
+        <q-card>
+          <q-card-section>
+            <q-item>
+              <q-item-section>
+                typeCode: {{ responses.typeCode }} <br>
+                reason: {{ responses.reason }} <br>
+                responseModel: {{ responses.responseModel }} <br>
+                headers: {{ responses.headers }} <br>
+              </q-item-section>
+              <q-item-section side>
+                <q-icon
+                  class="text-right"
+                  side
+                  name="edit"
+                  color="primary"
+                  @click.stop="edit()"
+                />
+              </q-item-section>
+              <q-item-section side>
+                <q-icon
+                  class="text-right"
+                  side
+                  name="delete"
+                  color="primary"
+                  @click.stop="dialogConfirmDeleteResponses = true; response = responses; tagIndex = indexTags; pathIndex = indexPath; responseIndex = indexResponse"
+                />
+              </q-item-section>
+            </q-item>
+          </q-card-section>
+        </q-card>
+      </q-expansion-item>
+      <q-separator />
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+export default {
+  props: {
+    responses: {
+      type: Object,
+      default: function () {
+        return {}
+      }
+    },
+    indexTags: {
+      type: Number,
+      default: 0
+    },
+    indexPath: {
+      type: Number,
+      default: 0
+    },
+    indexResponses: {
+      type: Number,
+      default: 0
+    }
+  },
+  methods: {
+    edit () {
+      this.$store.dispatch('setResponse', this.responses)
+      this.$store.dispatch('setTagIndex', this.indexTags)
+      this.$store.dispatch('setResponse', this.indexPath)
+      this.$store.dispatch('setResponse', this.indexResponses)
+      this.$emit('edit')
+      console.log(this.tag)
+      // this.$emit('edit')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
