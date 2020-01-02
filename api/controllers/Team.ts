a6c54d0efabd1396ee153767570a2e1a2ba103ee
joao <joaoserj@gmail.com>
2020-01-02 18:28:14 -0300

    p- add member by email

--- api/controllers/Team.ts
+++ api/controllers/Team.ts
@@ -69,21 +69,26 @@ class Team {
   }
 
   public async addMember (req: NewRequest, res: Response): Promise<any> {
-    const { teamIdFk, userIdFK } = req.body
+    const { teamIdFk, email } = req.body
     try {
       const team = await knex('team').select().where({
         id: teamIdFk,
         managerIdFk: req.userId
       })
+      const user = await knex('users').select().where({ email: email })
       if (team.length !== 0) {
-        if (team[0].managerIdFk === req.userId) {
-          const result = await knex('team_rules').insert({
-            teamIdFk: teamIdFk,
-            userIdFK: userIdFK
-          }).returning('*')
-          resp.returnSucessObject(res, result)
+        if (user.length !== 0) {
+          if (team[0].managerIdFk === req.userId) {
+            const result = await knex('team_rules').insert({
+              teamIdFk: teamIdFk,
+              userIdFK: user[0].id
+            }).returning('*')
+            resp.returnSucessObject(res, result)
+          } else {
+            resp.returnErrorMessage(res, 'Você não tem autorização para adicionar membros')
+          }
         } else {
-          resp.returnErrorMessage(res, 'Você não tem autorização para adicionar membros')
+          resp.returnErrorMessage(res, 'O email informado não pertence a nenhum usuário')
         }
       } else {
         resp.returnErrorMessage(res, 'O time informado não existe')
