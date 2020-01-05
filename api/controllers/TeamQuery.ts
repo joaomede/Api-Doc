578313d4953a805431a14af884e44e050a9396ae
joao <joaoserj@gmail.com>
2020-01-05 11:15:25 -0300

    p- created endpoints for TeamQuery and routes

--- api/controllers/TeamQuery.ts
+++ api/controllers/TeamQuery.ts
@@ -10,15 +10,24 @@ class ApiTeam {
   public async getApiAndEndPoints (req: NewRequest, res: Response): Promise<any> {
     const { id } = req.params
 
-    try {
-      const api = await knex('api').where({ id: id })
-      const tags = await knex('tags').where({ apiIdFk: id })
-      api[0].tags = tags
+    const rules = await knex('team_rules')
+      .where({ id: id, userIdFk: req.userId })
+      .join('teams', 'teams.id', 'team_rules.teamIdFk')
+      .select('teams.apiIdFk')
 
-      if (api.length === 0) {
-        resp.returnErrorMessage(res, 'A api que você está tentando acessar não foi encontrada')
+    try {
+      if (rules.length !== 0) {
+        const api = await knex('api').where({ id: rules[0].apiIdFk })
+        const tags = await knex('tags').where({ apiIdFk: rules[0].apiIdFk })
+        api[0].tags = tags
+
+        if (api.length === 0) {
+          resp.returnErrorMessage(res, 'A api que você está tentando acessar não foi encontrada')
+        } else {
+          resp.returnSucessObject(res, api[0])
+        }
       } else {
-        resp.returnSucessObject(res, api[0])
+        resp.returnErrorMessage(res, 'A equipe informada foi encontrada ou você não faz parte')
       }
     } catch (error) {
       resp.returnErrorMessage(res, 'Erro ao tentar carregar a documentação')
@@ -27,18 +36,27 @@ class ApiTeam {
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   public async getVerbsAndCodes (req: NewRequest, res: Response): Promise<any> {
-    const { endPointId } = req.params
+    const { id, endPointId } = req.params
+
+    const rules = await knex('team_rules')
+      .where({ id: id, userIdFk: req.userId })
+      .join('teams', 'teams.id', 'team_rules.teamIdFk')
+      .select('teams.apiIdFk')
 
     try {
-      const verbAndCodes = await knexPopulate(knex, 'paths')
-        .find({ tagsIdFk: endPointId })
-        .populate('responses', 'pathsIdFk', 'responses')
-        .exec()
+      if (rules.length !== 0) {
+        const verbAndCodes = await knexPopulate(knex, 'paths')
+          .find({ tagsIdFk: endPointId })
+          .populate('responses', 'pathsIdFk', 'responses')
+          .exec()
 
-      if (verbAndCodes.length === 0) {
-        resp.returnErrorMessage(res, 'Não há verbos disponíveis')
+        if (verbAndCodes.length === 0) {
+          resp.returnErrorMessage(res, 'Não há verbos disponíveis')
+        } else {
+          resp.returnSucessObject(res, verbAndCodes)
+        }
       } else {
-        resp.returnSucessObject(res, verbAndCodes)
+        resp.returnErrorMessage(res, 'A equipe informada foi encontrada ou você não faz parte')
       }
     } catch (error) {
       resp.returnErrorMessage(res, 'Erro ao tentar expandir')
@@ -46,47 +64,35 @@ class ApiTeam {
   }
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  public async apiList (req: NewRequest, res: Response): Promise<any> {
-    // const { sortValue, campo } = req.params
-
-    // esse metodo precisa fazer o SORT
+  public async listTeamsIAmMember (req: NewRequest, res: Response): Promise<any> {
     try {
-      const allApiByUser = await knex('api').select().where({ userIdFk: req.userId })
-      resp.returnSucessObject(res, allApiByUser)
+      const teamsAmMember = await knex('team_rules')
+        .where({ 'team_rules.userIdFk': req.userId })
+        .join('teams', 'teams.id', 'team_rules.teamIdFk')
+        .join('api', 'api.id', 'teams.apiIdFk')
+        .join('users', 'teams.managerIdFk', 'users.id')
+        .select('team_rules.id', 'teams.teamName', 'api.apiName', 'users.name')
+      resp.returnSucessObject(res, teamsAmMember)
     } catch (error) {
       resp.returnErrorMessage(res, "Erro ao carregar a lista de Api's")
     }
   }
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  public async pathIndex (req: NewRequest, res: Response): Promise<any> {
-    const { endPointId } = req.params
-
-    if (endPointId === undefined || endPointId === null) {
-      resp.returnErrorMessage(res, 'Não foi identificado o path relacionado')
-    }
+  public async exitTeam (req: NewRequest, res: Response): Promise<any> {
+    const { id } = req.params
 
     try {
-      const path = await knex('paths').where({ tagsIdFk: endPointId })
-      resp.returnSucessObject(res, path)
+      const result = await knex('team_rules').where({ id: id, userIdFk: req.userId })
+
+      if (result.length !== 0) {
+        await knex('team_rules').where({ id: id, userIdFk: req.userId }).del()
+        resp.returnSucessMessage(res, 'Você saiu dessa equipe')
+      } else {
+        resp.returnErrorMessage(res, 'Erro ao tentar sair desta equipe')
+      }
     } catch (error) {
-      resp.returnErrorMessage(res, 'Erro ao tentar carregar todos os EndPoints')
-    }
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  public async responseIndex (req: NewRequest, res: Response): Promise<any> {
-    const { verbId } = req.params
-
-    if (verbId === undefined || verbId === null) {
-      resp.returnErrorMessage(res, 'Verbo Associado não foi referênciado')
-    }
-
-    try {
-      const tags = await knex('responses').where({ pathsIdFk: verbId })
-      resp.returnSucessObject(res, tags)
-    } catch (error) {
-      resp.returnErrorMessage(res, 'Erro ao tentar carregar todos os códigos respostas')
+      resp.returnErrorMessage(res, 'Erro ao tentar sair da equipe')
     }
   }
 }
