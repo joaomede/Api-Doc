a2bbe18c438701590ce02892f912f410f15bce97
joao <joaoserj@gmail.com>
2020-01-04 21:45:13 -0300

    p- add list all private api on create new team

--- view/src/components/dialog/addDialog/DialogAddTeam.vue
+++ view/src/components/dialog/addDialog/DialogAddTeam.vue
@@ -2,6 +2,7 @@
   <q-dialog
     v-model="dialogPopup"
     @hide="eventClose()"
+    @show="listAllPrivateAPI()"
   >
     <q-card>
       <q-card-section>
@@ -21,13 +22,12 @@
       </q-card-section>
 
       <q-card-section>
-        <q-form class="q-gutter-md">
-          <q-input
-            v-model="form.apiIdFk"
-            label="Informe a id da Documentação"
-            required
-          />
-        </q-form>
+        <q-select
+          v-model="form.apiIdFk"
+          filled
+          :options="listApis"
+          label="Lista de Api Privadas"
+        />
       </q-card-section>
 
       <q-card-section align="center">
@@ -63,7 +63,9 @@ export default {
         apiIdFk: '',
         teamName: ''
       },
-      dialogPopup: this.dialog
+      dialogPopup: this.dialog,
+
+      listApis: []
     }
   },
   watch: {
@@ -79,7 +81,22 @@ export default {
       this.reset()
     },
     save () {
+      this.form.apiIdFk = this.form.apiIdFk.value
       this.$emit('save', this.form)
+    },
+    async listAllPrivateAPI () {
+      try {
+        const result = await this.$axios.get(`api/api/getallprivate`, { headers: this.user.headers })
+        this.listApis = []
+        result.data.forEach(element => {
+          this.listApis.push({
+            label: element.apiName,
+            value: element.id
+          })
+        })
+      } catch (error) {
+        this.$notify(error.response.data.error, 'red')
+      }
     },
     reset () {
       this.form = {
