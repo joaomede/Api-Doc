6f735b4aa3d038b61f73babbee643b492e2f26cc
joao <joaoserj@gmail.com>
2020-01-13 22:26:50 -0300

    p- fix install.sh

--- install.sh
+++ install.sh
@@ -10,6 +10,6 @@ migrate='yarn migrate'
 
 $dockerRun -v $(pwd):$workDir $dockerImage $install
 $dockerRun -v $(pwd):$workDir $dockerImage $buildApi
+cd view
 $dockerRun -v $(pwd):$workDir $dockerImage $install
-cd ..
 $dockerRun -v $(pwd):$workDir $dockerImage $buildView
