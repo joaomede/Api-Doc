6ccfaa6db2bc7b45cdbe1c268c4f3c7772fa2076
Joao Medeiros <joaoserj@gmail.com>
2020-01-14 16:51:32 -0300

    p- update i18 boot

--- view/src/boot/i18n.js
+++ view/src/boot/i18n.js
@@ -1,13 +1,17 @@
 import VueI18n from 'vue-i18n'
-
 import messages from 'src/i18n'
+import Vue from 'vue'
+
+Vue.use(VueI18n)
+
+let i18n = new VueI18n({
+  locale: 'en-us',
+  fallbackLocale: 'en-us',
+  messages
+})
 
 export default ({ app, Vue }) => {
-  Vue.use(VueI18n)
-
-  app.i18n = new VueI18n({
-    locale: 'en-us',
-    fallbackLocale: 'en-us',
-    messages
-  })
+  app.i18n = i18n
 }
+
+export { i18n }
