7a46ee1370bdb6d401e978d3f183767fabafdc67
Joao Medeiros <joaoserj@gmail.com>
2020-01-23 09:38:29 -0300

    p- improves JWT

--- api/middlewares/CheckJwt.ts
+++ api/middlewares/CheckJwt.ts
@@ -11,28 +11,28 @@ class CheckJwt {
   public checkJwt (req: NewRequest, res: Response, next: NextFunction):void | Response {
     const authHeader = req.headers.authorization
     let jwtPayload
-    if (!authHeader) {
+    if (authHeader === undefined || authHeader === null) {
       resp.returnErrorCode(res, 401, 'No token provided')
-    }
+    } else {
+      // Bearer klasjdasodajs5fd4s6
+      const parts: Array<string> = authHeader.split(' ')
+      if (parts.length !== 2) {
+        resp.returnErrorCode(res, 401, 'Token error')
+      }
 
-    // Bearer klasjdasodajs5fd4s6
-    const parts: Array<string> = authHeader.split(' ')
-    if (parts.length !== 2) {
-      resp.returnErrorCode(res, 401, 'Token error')
-    }
+      const [scheme, token] = parts
 
-    const [scheme, token] = parts
+      if (!/^Bearer$/i.test(scheme)) {
+        resp.returnErrorCode(res, 401, 'Token malformatted')
+      }
 
-    if (!/^Bearer$/i.test(scheme)) {
-      resp.returnErrorCode(res, 401, 'Token malformatted')
-    }
-
-    try {
-      jwtPayload = jwt.verify(token, secret)
-      req.userId = jwtPayload.id
-      return next()
-    } catch (error) {
-      resp.returnErrorCode(res, 401, 'Token invalid')
+      try {
+        jwtPayload = jwt.verify(token, secret)
+        req.userId = jwtPayload.id
+        return next()
+      } catch (error) {
+        resp.returnErrorCode(res, 401, 'Token invalid')
+      }
     }
   }
 }
