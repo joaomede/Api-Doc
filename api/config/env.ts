11cac44ab49fd4b7c2e2450dcd1f489629699e79
Joao Medeiros <joaoserj@gmail.com>
2020-01-29 11:46:00 -0300

    update env config

--- api/config/env.ts
+++ api/config/env.ts
@@ -2,7 +2,7 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 
 export const env = {
-  port: process.env.PORTHOST as unknown as number,
+  port: 80,
   postgresUser: process.env.POSTGRES_USER,
   postgresPassword: process.env.POSTGRES_PASSWORD,
   postgresDatabase: process.env.POSTGRES_DB,
