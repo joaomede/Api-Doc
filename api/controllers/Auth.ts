511d5ba819623cf649d9b6782a4953701cf0ea31
Joao Medeiros <joaoserj@gmail.com>
2020-02-04 12:26:43 -0300

    disable change password

--- api/controllers/Auth.ts
+++ api/controllers/Auth.ts
@@ -26,41 +26,41 @@ class Auth {
   }
 
   public async changePassword (req: NewRequest, res: Response): Promise<void> {
-    const { passwordOne, passwordTwo } = req.body
-    const authHeader = req.headers.authorization
+    // const { passwordOne, passwordTwo } = req.body
+    // const authHeader = req.headers.authorization
 
-    async function changePass (decoded): Promise<void> {
-      try {
-        const password = await bcrypt.hash(passwordOne, 10)
-        await knex('users').where({ id: decoded.id }).update({ password: password })
-        resp.returnSucessMessage(res, 'Senha alterada com sucesso')
-      } catch (error) {
-        resp.returnErrorMessage(res, 'Erro ao tentar alterar a senha')
-      }
-    }
+    // async function changePass (decoded): Promise<void> {
+    //   try {
+    //     const password = await bcrypt.hash(passwordOne, 10)
+    //     await knex('users').where({ id: decoded.id }).update({ password: password })
+    //     resp.returnSucessMessage(res, 'Senha alterada com sucesso')
+    //   } catch (error) {
+    //     resp.returnErrorMessage(res, 'Erro ao tentar alterar a senha')
+    //   }
+    // }
 
-    if (authHeader === undefined || authHeader === null) {
-      resp.returnErrorMessage(res, 'Você preecisa estar logado para fazer isso')
-    }
+    // if (authHeader === undefined || authHeader === null) {
+    //   resp.returnErrorMessage(res, 'Você preecisa estar logado para fazer isso')
+    // }
 
-    if (passwordOne !== passwordTwo) {
-      resp.returnErrorMessage(res, 'As senhas não são iguais')
-    } else {
-      const parts = authHeader.split(' ')
-      if (parts.length !== 2) {
-        resp.returnErrorCode(res, 401, 'Token error')
-      }
-      const [scheme, token] = parts
-      if (!/^Bearer$/i.test(scheme)) {
-        resp.returnErrorCode(res, 401, 'Token mal formado')
-      }
-      jwt.verify(token, env.secret, (err, decoded) => {
-        if (err) {
-          resp.returnErrorCode(res, 403, 'Token invalido')
-        }
-        changePass(decoded)
-      })
-    }
+    // if (passwordOne !== passwordTwo) {
+    //   resp.returnErrorMessage(res, 'As senhas não são iguais')
+    // } else {
+    //   const parts = authHeader.split(' ')
+    //   if (parts.length !== 2) {
+    //     resp.returnErrorCode(res, 401, 'Token error')
+    //   }
+    //   const [scheme, token] = parts
+    //   if (!/^Bearer$/i.test(scheme)) {
+    //     resp.returnErrorCode(res, 401, 'Token mal formado')
+    //   }
+    //   jwt.verify(token, env.secret, (err, decoded) => {
+    //     if (err) {
+    //       resp.returnErrorCode(res, 403, 'Token invalido')
+    //     }
+    //     changePass(decoded)
+    //   })
+    // }
   }
 }
 
