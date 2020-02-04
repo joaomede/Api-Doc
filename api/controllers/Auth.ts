9929bea0f86ddd35216ccd165e321b49bdaa98d8
Joao Medeiros <joaoserj@gmail.com>
2020-02-04 11:20:22 -0300

    implemented sdk in Auth.ts

--- api/controllers/Auth.ts
+++ api/controllers/Auth.ts
@@ -1,31 +1,14 @@
 import apiDoc from '../config/api'
 import { Response, Request } from 'express'
-import Plugin from '../plugin/Plugin'
 import resp from 'resp-express'
 import { NewRequest } from '../interface/NewRequest'
-import { env } from '../config/env'
-import * as jwt from 'jsonwebtoken'
-import bcrypt = require('bcrypt')
 
 class Auth {
   public async register (req: Request, res: Response): Promise<void> {
-    const { email, password } = req.body
+    const { name, email, password } = req.body
     try {
-      const result = await knex('users').select().where('email', email)
-      if (result.length === 0) {
-        const saltRounds = 10
-        const hash = bcrypt.hashSync(password, saltRounds)
-        req.body.password = hash
-
-        try {
-          const user = await knex('users').insert(req.body).returning('*')
-          resp.returnSucessObject(res, user)
-        } catch (error) {
-          resp.returnErrorMessage(res, 'Erro ao tentar cadastrar o novo usuário')
-        }
-      } else {
-        resp.returnErrorMessage(res, 'O email já existe')
-      }
+      const user = await apiDoc.userRegister(name, email, password)
+      resp.returnSucessObject(res, user)
     } catch (error) {
       resp.returnErrorMessage(res, 'Erro ao verificar disponibilidade do e-mail')
     }
@@ -35,23 +18,10 @@ class Auth {
     const { email, password } = req.body
 
     try {
-      const user = await knex('users').where({ email: email })
-      if (user.length === 0) {
-        resp.returnErrorMessage(res, 'O usuário não foi encontrado')
-      } else {
-        const match = bcrypt.compareSync(password, user[0].password)
-        if (match) {
-          user[0].password = undefined
-          resp.returnSucessObject(res, {
-            user: user[0],
-            token: await Plugin.generateToken({ id: user[0].id }, '7d')
-          })
-        } else {
-          resp.returnErrorMessage(res, 'A senhão está incorreta')
-        }
-      }
+      const user = await apiDoc.userAuth(email, password)
+      resp.returnSucessObject(res, user)
     } catch (error) {
-      resp.returnErrorMessage(res, 'Problemas ao tentar efetuar o login')
+      resp.returnErrorMessage(res, error.message)
     }
   }
 
