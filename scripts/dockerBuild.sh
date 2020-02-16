1a1808e797d2133e3468f23a430c501e9d2414f4
joao <joaoserj@gmail.com>
2020-02-16 11:33:27 -0300

    update script build docker

--- scripts/dockerBuild.sh
+++ scripts/dockerBuild.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 ## By João Medeiros - <symbol2studio@gmail.com>
+dockerRun='docker run -it --rm'
+dockerImage='joaomede/nodejs12prod:1.0'
+workDir='/usr/src/app'
+install='yarn -i'
+installProd='yarn -i --production'
+
+
 
 read -p "New Tag Version: " version
 
-yarn build
-yarn build:view
+sudo $dockerRun -v $(pwd):$workDir $dockerImage $install
+sudo $dockerRun -v $(pwd)/view:$workDir $dockerImage $install
+sudo $dockerRun -v $(pwd):$workDir $dockerImage yarn build:view
+sudo $dockerRun -v $(pwd):$workDir $dockerImage yarn build
+sudo rm -r node_modules
+sudo rm yarn.lock
+sudo $dockerRun -v $(pwd):$workDir $dockerImage $installProd
 sudo docker build -t joaomede/apidoc:${version} . 
