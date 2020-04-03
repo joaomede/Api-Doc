8979f212ef2a7aa117d544f86032f3de3a3907cd
joao <joaoserj@gmail.com>
2020-04-02 21:51:22 -0300

    fixed problem main menu column

--- view/src/components/menu/MainMenu.vue
+++ view/src/components/menu/MainMenu.vue
@@ -1,74 +1,77 @@
 <template>
   <q-card
     class="row justify-center"
-    style="min-height: 250px"
+    style="min-height: 200px"
   >
-    <div class="text-h5 text-center col-12">
+    <div class="text-h5 text-center">
       {{ title }}
     </div>
-    <q-btn
-      v-ripple
-      class="btnMenu"
-      to="/publicdocs"
-      dense
-    >
-      <q-icon
-        name="list"
-        class="text-black"
-        style="font-size: 4.4em;"
-      />
-      <div class="text-p">
-        Public API's
-      </div>
-    </q-btn>
 
-    <q-btn
-      v-ripple
-      class="btnMenu"
-      to="/privatedocs"
-      dense
-    >
-      <q-icon
-        name="security"
-        class="text-black"
-        style="font-size: 4.4em;"
-      />
-      <div class="text-p">
-        My API's
-      </div>
-    </q-btn>
+    <div>
+      <q-btn
+        v-ripple
+        class="btnMenu"
+        to="/publicdocs"
+        dense
+      >
+        <q-icon
+          name="list"
+          class="text-black"
+          style="font-size: 4.4em;"
+        />
+        <div class="text-p">
+          Public API's
+        </div>
+      </q-btn>
 
-    <q-btn
-      v-ripple
-      class="btnMenu"
-      to="/teamdocs"
-      dense
-    >
-      <q-icon
-        name="list"
-        class="text-black"
-        style="font-size: 4.4em;"
-      />
-      <div class="text-p">
-        My Team Api
-      </div>
-    </q-btn>
+      <q-btn
+        v-ripple
+        class="btnMenu"
+        to="/privatedocs"
+        dense
+      >
+        <q-icon
+          name="security"
+          class="text-black"
+          style="font-size: 4.4em;"
+        />
+        <div class="text-p">
+          My API's
+        </div>
+      </q-btn>
 
-    <q-btn
-      v-ripple
-      class="btnMenu"
-      to="/manageteams"
-      dense
-    >
-      <q-icon
-        name="people"
-        class="text-black"
-        style="font-size: 4.4em;"
-      />
-      <div class="text-p">
-        Manage Teams
-      </div>
-    </q-btn>
+      <q-btn
+        v-ripple
+        class="btnMenu"
+        to="/teamdocs"
+        dense
+      >
+        <q-icon
+          name="list"
+          class="text-black"
+          style="font-size: 4.4em;"
+        />
+        <div class="text-p">
+          My Team Api
+        </div>
+      </q-btn>
+
+      <q-btn
+        v-ripple
+        class="btnMenu"
+        to="/manageteams"
+        dense
+      >
+        <q-icon
+          name="people"
+          class="text-black"
+          style="font-size: 4.4em;"
+        />
+        <div class="text-p">
+          Manage Teams
+        </div>
+      </q-btn>
+    </div>
   </q-card>
 </template>
 
