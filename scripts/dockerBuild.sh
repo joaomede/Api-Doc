8d6185a9ff4c84488b7bc2fe4aeaed3fe47cdcda
joao <joaoserj@gmail.com>
2020-04-12 21:09:35 -0300

    updated script docker build

--- scripts/dockerBuild.sh
+++ scripts/dockerBuild.sh
@@ -10,15 +10,16 @@ installProd='yarn -i --production'
 
 read -p "New Tag Version: " version
 
+sudo rm -r node_modules yarn.lock view/dist
 sudo $dockerRun -v $(pwd):$workDir $dockerImage $install
 sudo $dockerRun -v $(pwd)/view:$workDir $dockerImage $install
 sudo $dockerRun -v $(pwd):$workDir $dockerImage yarn build:view
 sudo $dockerRun -v $(pwd):$workDir $dockerImage yarn build
-sudo rm -r node_modules
-sudo rm yarn.lock
+sudo rm -r node_modules yarn.lock
 sudo $dockerRun -v $(pwd):$workDir $dockerImage $installProd
 sudo docker build -t joaomede/apidoc:${version} .
 
-sudo rm -r node_modules
-sudo rm yarn.lock
+sudo rm -r node_modules yarn.lock
 sudo $dockerRun -v $(pwd):$workDir $dockerImage $install
+
+sudo docker tag joaomede/apidoc:${version} joaomede/apidoc:latest
