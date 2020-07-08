2df8993ead65c518e0610ff1a38ffd49d37b66e8
João Medeiros <joaoserj@gmail.com>
2020-07-08 14:13:40 -0300

    update eslint rules

--- view/.eslintrc.js
+++ view/.eslintrc.js
@@ -37,7 +37,7 @@ module.exports = {
     // allow paren-less arrow functions
     "arrow-parens": "off",
     "one-var": "off",
-
+    "no-return-assign": "off",
     "import/first": "off",
     "import/named": "error",
     "import/namespace": "error",
