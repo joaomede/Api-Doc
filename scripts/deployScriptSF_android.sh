9bafc7eaa92413cfab23b63fb293618097248f2f
João Medeiros <joaoserj@gmail.com>
2021-02-16 23:19:28 -0300

    Update deployScriptSF_android.sh

--- scripts/deployScriptSF_android.sh
+++ scripts/deployScriptSF_android.sh
@@ -12,7 +12,7 @@ scp "${pathNewFile}" "${SCPserver}"
 
 queryPut="default=android"
 headers="Accept: application/json"
-token="api_key=6e49c195-52fd-4c35-9c29-074b0395ee2c"
+token="api_key="
 url="https://sourceforge.net/projects/api-doc/files/ApiDoc-${version}${formatApk}"
 
 curl -H "${headers}" -X PUT -d "${queryPut}" -d "${token}" "${url}"
